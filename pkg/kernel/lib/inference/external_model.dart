d78f1e968e231467e141ae77030b6fd2021e448c
Asger Feldthaus <asgerf@google.com>
2017-02-01 12:56:51 +0100

    Add the missing external_model.dart file

---
+++ pkg/kernel/lib/inference/external_model.dart
@@ -0,0 +1,99 @@
+import 'package:kernel/ast.dart';
+import 'package:kernel/core_types.dart';
+
+abstract class ExternalModel {
+  /// True if [member] does not return `null` and does not cause any objects
+  /// to escape.
+  bool isNicelyBehaved(Member member);
+}
+
+class BasicExternalModel extends ExternalModel {
+  final CoreTypes coreTypes;
+  Class externalNameAnnotation;
+
+  static final Set<String> niceMembers = new Set<String>.from([
+    // doubles
+    'Double_add',
+    'Double_ceil',
+    'Double_div',
+    'Double_doubleFromInteger',
+    'Double_equal',
+    'Double_equalToInteger',
+    'Double_flipSignBit',
+    'Double_floor',
+    'Double_getIsInfinite',
+    'Double_getIsNaN',
+    'Double_getIsNegative',
+    'Double_greaterThan',
+    'Double_greaterThanFromInteger',
+    'Double_modulo',
+    'Double_mul',
+    'Double_remainder',
+    'Double_round',
+    'Double_sub',
+    'Double_toInt',
+    'Double_toString',
+    'Double_toStringAsExponential',
+    'Double_toStringAsFixed',
+    'Double_toStringAsPrecision',
+    'Double_trunc_div',
+    'Double_truncate',
+
+    // integers
+    'Integer_addFromInteger',
+    'Integer_bitAndFromInteger',
+    'Integer_bitOrFromInteger',
+    'Integer_bitXorFromInteger',
+    'Integer_equalToInteger',
+    'Integer_fromEnvironment',
+    'Integer_greaterThanFromInteger',
+    'Integer_moduloFromInteger',
+    'Integer_mulFromInteger',
+    'Integer_subFromInteger',
+    'Integer_truncDivFromInteger',
+
+    // strings
+    'String_charAt',
+    'String_codeUnitAt',
+    'String_concat',
+    'String_concatRange',
+    'String_fromEnvironment',
+    'String_getHashCode',
+    'String_getLength',
+    'String_toLowerCase',
+    'String_toUpperCase',
+    'StringBase_createFromCodePoints',
+    'StringBase_joinReplaceAllResult',
+    'StringBase_substringUnchecked',
+    'StringBuffer_createStringFromUint16Array',
+    'StringToSystemEncoding',
+  ]);
+
+  BasicExternalModel(this.coreTypes) {
+    externalNameAnnotation =
+        coreTypes.getCoreClass('dart:_internal', 'ExternalName');
+  }
+
+  ConstructorInvocation getAnnotation(
+      List<Expression> annotations, Class class_) {
+    for (var annotation in annotations) {
+      if (annotation is ConstructorInvocation &&
+          annotation.target.enclosingClass == class_) {
+        return annotation;
+      }
+    }
+    return null;
+  }
+
+  String getAsString(Expression node) {
+    return node is StringLiteral ? node.value : null;
+  }
+
+  bool isNicelyBehaved(Member member) {
+    var annotation = getAnnotation(member.annotations, externalNameAnnotation);
+    if (annotation == null) return false;
+    if (annotation.arguments.positional.length < 1) return false;
+    String name = getAsString(annotation.arguments.positional[0]);
+    return niceMembers.contains(name);
+  }
+}
