486c72f2760deb7792ba9ad54218d7088d0f353d
Asger Feldthaus <asgerf@google.com>
2017-02-21 12:48:56 +0100

    Add copyright

--- pkg/kernel/test/strong_inference/dump.dart
+++ pkg/kernel/test/strong_inference/dump.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 import 'package:kernel/inference/extractor/constraint_extractor.dart';
 import 'package:kernel/inference/solver/solver.dart';
 import 'package:kernel/kernel.dart';
