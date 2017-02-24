8db52e69837716c3884c45e39efe7caa70f73a74
Asger Feldthaus <asgerf@google.com>
2017-02-24 13:47:22 +0100

    Fix for the fix

--- pkg/front_end/lib/src/fasta/source/diet_listener.dart
+++ pkg/front_end/lib/src/fasta/source/diet_listener.dart
@@ -560,7 +560,7 @@ class DietListener extends StackListener {
 
   bool get isTargetingDartVm {
     // TODO(ahe): Find a more reliable way to check if this is the Dart VM.
-    return coreTypes.containsLibrary("dart:_js_helper");
+    return !coreTypes.containsLibrary("dart:_js_helper");
   }
 
   @override
