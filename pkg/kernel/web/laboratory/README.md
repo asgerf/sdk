cf3df692c5e48c390e17e8dfc57f4bd5a4e0e6e6
Asger Feldthaus <asgerf@google.com>
2017-03-10 18:01:12 +0100

    Add WIP viewer

---
+++ pkg/kernel/web/laboratory/README.md
@@ -0,0 +1,6 @@
+Laboratory is a web page for inspecting static compilation artifacts from
+AOT compilers, and can be thought of as "observatory for static things".
+
+It is intended to look and feel like Observatory, so our users get a coherent
+suite of performance diagnostics tools instead of separate tools each with
+their own style.
---
+++ pkg/kernel/web/laboratory/laboratory.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+import 'dart:html';
+import 'dart:typed_data';
+import 'package:kernel/ast.dart';
+import 'package:kernel/binary/ast_from_binary.dart';
+import 'package:kernel/inference/report/binary_reader.dart';
+import 'package:kernel/inference/report/report.dart';
+import 'package:kernel/inference/constraints.dart';
+import 'package:kernel/util/reader.dart';
+
+FileUploadInputElement reportFileInput =
+    document.getElementById('report-file-input');
+
+FileUploadInputElement kernelFileInput =
+    document.getElementById('kernel-file-input');
+
+DivElement debugBox = document.getElementById('debug-box');
+
+Program program;
+ConstraintSystem constraintSystem;
+Report report;
+
+info(message) {
+  print(message);
+}
+
+main() {
+  kernelFileInput.onChange.listen(onKernelFileChange);
+  reportFileInput.onChange.listen(onReportFileChange);
+}
+
+Future<Uint8List> readBytesFromFileInput(FileUploadInputElement input) async {
+  if (input.files.isEmpty) return null;
+  var file = input.files.first;
+  if (file == null) return null;
+  FileReader reader = new FileReader();
+  reader.readAsArrayBuffer(file);
+  await reader.onLoadEnd.first;
+  return reader.result as Uint8List;
+}
+
+onKernelFileChange(Event event) async {
+  var bytes = await readBytesFromFileInput(kernelFileInput);
+  if (bytes == null) return;
+  program = new Program();
+  new BinaryBuilder(bytes).readProgram(program);
+  info('Read kernel file with ${program.libraries.length} libraries');
+}
+
+onReportFileChange(Event event) async {
+  if (program == null) {
+    info('Load the program first');
+    return;
+  }
+  var bytes = await readBytesFromFileInput(reportFileInput);
+  if (bytes == null) return;
+  var reader = new BinaryReportReader(new Reader(bytes, program.root));
+  constraintSystem = reader.readConstraintSystem();
+  var events = reader.readEventList();
+  report = new Report.fromTransfers(events);
+  info('Read report with '
+      '${reader.constraintSystem.numberOfConstraints} constraints and '
+      '${events.length} transfer events');
+}
