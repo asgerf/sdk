914cbbca84c6a24c98fbddf7750b63366319988a
Asger Feldthaus <asgerklasker@gmail.com>
2017-03-11 12:05:22 +0100

    Add missing keycodes.dart

---
+++ pkg/kernel/web/laboratory/keycodes.dart
@@ -0,0 +1,13 @@
+library kernel.laboratory.keycodes;
+
+class KeyCodes {
+  static const int backspace = 8;
+  static const int tab = 9;
+  static const int enter = 13;
+  static const int escape = 27;
+  static const int space = 32;
+  static const int leftArrow = 37;
+  static const int upArrow = 38;
+  static const int rightArrow = 39;
+  static const int downArrow = 40;
+}
