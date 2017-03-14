50b238d8b927a7b156146404553a3f0e86384156
Asger Feldthaus <asgerf@google.com>
2017-03-14 12:47:15 +0100

    Fix copyright and library tags

--- pkg/kernel/web/laboratory/laboratory_data.dart
+++ pkg/kernel/web/laboratory/laboratory_data.dart
@@ -1,4 +1,7 @@
-library laboratory_data;
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+library kernel.laboratory.data;
 
 import 'package:kernel/ast.dart';
 import 'package:kernel/class_hierarchy.dart';
--- pkg/kernel/web/laboratory/lexer.dart
+++ pkg/kernel/web/laboratory/lexer.dart
@@ -1,4 +1,7 @@
-library laboratory.lexer;
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+library kernel.laboratory.lexer;
 
 import 'package:front_end/src/scanner/errors.dart';
 import 'package:front_end/src/scanner/reader.dart';
